5f5dd8594c69f02a488bc64cd939bd8d44bd8d3a
kagankose <skagankose@Suha-Kagans-Macbook.local>
2017-10-24 12:20:39 +0200

    LR w/ Accuracy .82

--- Suha's/custom_helpers.py
+++ Suha's/custom_helpers.py
@@ -3,7 +3,39 @@
 import csv
 import numpy as np
 
-"""From exercise 3"""
+def replace_nan_with_frequent(x):
+    """replace nan values with the most frequent values within the column"""
+    for i in range(x.shape[1]):
+        if np.any(x[:, i] == -999):
+            tmp = (x[:, i] != -999)
+            values, counts = np.unique(x[tmp, i], return_counts = True)
+            if (len(values) > 1):
+                x[~tmp, i] = values[np.argmax(counts)]
+            else:
+                x[~tmp, i] = 0
+    return x
+
+def convert_categorical(x, column_num):
+    """convert a column to one-hot representation"""
+    arr = x[:,column_num]
+
+    cat_reference = dict()
+    cat_unique = np.unique(arr)
+
+    for i, cat in enumerate(cat_unique):
+        code =  len(cat_unique) * [0]
+        code[i] = 1
+        cat_reference[cat] = code
+
+
+    cat_columns = list()
+    for cat in arr:
+        cat_columns.append(cat_reference[cat])
+
+    x = np.delete(x, (column_num), axis=1)
+
+    return x, cat_columns
+
 def split_data(x, y, ratio, seed=1):
     """split the dataset based on the split ratio."""
     # set seed
@@ -21,14 +53,47 @@ def split_data(x, y, ratio, seed=1):
     y_te = y[index_te]
     return x_tr, x_te, y_tr, y_te
 
-def build_poly(x, degree):
-    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+def poly_features(x, degree):
+    """degrees of existing features"""
     poly = np.ones((len(x), 1))
-    for deg in range(1, degree+1):
+    for deg in range(1, degree + 1):
         poly = np.c_[poly, np.power(x, deg)]
-    return poly
 
-"""From exercise 2"""
+    return poly[:,1:]
+
+def cross_column_features(x, num_features=0):
+    """cross-column features created by combining existing ones"""
+
+    # 2s combination of columns
+    if not num_features:
+        num_features = x.shape[1]
+
+    double_features = list()
+    for i in range(num_features):
+        # print("Feature(%d/%d)" % (i,num_features-1)) #DEBUG
+        for j in range(i+1, num_features):
+            tmp = x[:,i] * x[:,j]
+            double_features.append(tmp)
+
+    double_features = np.array(double_features).T
+
+    return np.c_[x, double_features]
+
+    '''
+    # 3s combination of columns
+    num_double_features = len(double_features)
+    triple_features = list()
+    for i in range(num_features):
+        # print("Feature(%d/%d)" % (i, num_features-1)) #DEBUG
+        for j in range(double_features.shape[1]):
+            tmp = x[:, i] * double_features[:, j]
+            triple_features.append(tmp)
+
+    triple_features = np.array(triple_features).T
+
+    return np.c_[x, double_features, triple_features]
+    '''
+
 def standardize(x):
     """Standardize the original data set."""
     mean_x = np.mean(x)
@@ -37,14 +102,17 @@ def standardize(x):
     x = x / std_x
     return x, mean_x, std_x
 
-def build_model_data(height, weight, gender):
-    """Form (y,tX) to get regression data in matrix form."""
-    y = gender
-    x1 = height
-    x2 = weight
-    num_samples = len(y)
-    tx = np.c_[np.ones(num_samples), x1, x2]
-    return y, tx
+def standardize_columns(x):
+    """standardize all the columns of given dataset"""
+    num_samples = len(x[:,0])
+    num_features = len(x[0,:])
+    tx = np.zeros(num_samples)
+    for i in range(num_features):
+        # print("Standardize(%d/%d)"%(i,num_features-1)) #DEBUG
+        tmp,_,_ = standardize(x[:,i])
+        tx = np.c_[tx, tmp]
+
+    return tx[:,1:]
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     """
@@ -71,7 +139,6 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
 
-"""Already here"""
 def load_csv_data(data_path, sub_sample=False):
     """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
     y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
@@ -81,7 +148,8 @@ def load_csv_data(data_path, sub_sample=False):
 
     # convert class labels from strings to binary (-1,1)
     yb = np.ones(len(y))
-    yb[np.where(y=='b')] = -1
+    # yb[np.where(y=='b')] = -1
+    yb[np.where(y=='b')] = 0
 
     # sub-sample
     if sub_sample:
@@ -92,14 +160,13 @@ def load_csv_data(data_path, sub_sample=False):
     return yb, input_data, ids
 
 
-def predict_labels(weights, data):
+def predict_labels(weights, data, pos=1, neg=-1):
     """Generates class predictions given weights, and a test data matrix"""
     y_pred = np.dot(data, weights)
-    y_pred[np.where(y_pred <= 0)] = -1
-    y_pred[np.where(y_pred > 0)] = 1
+    y_pred[np.where(y_pred <= 0)] = neg
+    y_pred[np.where(y_pred > 0)] = pos
 
     return y_pred
-
 
 def create_csv_submission(ids, y_pred, name):
     """
